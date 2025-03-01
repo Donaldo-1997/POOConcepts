939086c29041488de5c189373b0739659d437081
donaldojbm7 <donaldojbm7@gmail.com>
2025-02-28 22:44:09 -0500

    created class Date

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,12 @@
+﻿using POOConcepts;
+
+
+try {
+    var dat1 = new Date();
+    var dat2 = new Date(2020, 2, 29);
+    Console.WriteLine(dat1);
+    Console.WriteLine(dat2);
+
+} catch(Exception e) {
+    Console.WriteLine(e);
+}
